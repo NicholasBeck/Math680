636b425129d74063fcb75903bf43ddf2293f5255
DOOM\Nicholas <nicholas.rudolph.beck@gmail.com>
2016-11-29 16:31:26 -0500

    made modifications to fhats

--- Code/TweedieKERE.R
+++ Code/TweedieKERE.R
@@ -13,18 +13,18 @@ source("kernels.R")
 source("kernelmatrix.R")
 sourceCpp('NewtonRcpp.cpp')
 
-TweedieKERE <- function(X, Y, lambda=1, rho=1.5, phi=1, sig=1){
+TweedieKERE <- function(X, Y, lambda=1, rho=1.5, phi=1, sig=1, n){
   ## Setup the kernel matrix
   kern <- rbfdot(sigma=sig)
   Kmat <- kernelMatrix(kern,X) ## symemtric matrix, k(x_i,x_j) = k(x_j,x_i)
   
   ## Setup more values
-  dimsX <- dim(X) ## Row, Col
-  alpha <- seq(0, 1, length.out=dimsX[1])
-  ## CALL THE C CODE
+  n <- nrow(X) ## Row, Col
+  alpha <- seq(0, 1, length.out=n)
   
-  NRCpp(Kmat,alpha,Y,rho,lamda)
+  ## CALL THE C CODE
+  NRCpp(Kmat,alpha,Y,rho,lambda,n)
 
 }
-
-Tweedie(X,Y)
+TweedieKERE(X,Y)
+system.time(TweedieKERE(X,Y))
