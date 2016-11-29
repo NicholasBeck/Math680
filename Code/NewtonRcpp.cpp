26edc51eb2596f49eb177de5c99b29c146390d07
DOOM\Nicholas <nicholas.rudolph.beck@gmail.com>
2016-11-29 17:06:13 -0500

    modifications to fhats

--- Code/NewtonRcpp.cpp
+++ Code/NewtonRcpp.cpp
@@ -4,31 +4,36 @@ using namespace arma;
 
 // [[Rcpp::depends(RcppArmadillo)]]
 // [[Rcpp::export]]
-arma::vec gradCpp(arma::mat Kmat, arma::vec alpha, arma::vec Y, double rho, double lambda){
-  arma::vec fhats = Kmat * alpha;
-  return Kmat*(exp((2-rho)*fhats)-Y%exp((1-rho)*fhats)+2*lambda*alpha);
+arma::vec fh(arma::mat Kmat, arma::vec alpha){
+  return Kmat * alpha;
+}
+
+// [[Rcpp::depends(RcppArmadillo)]]
+// [[Rcpp::export]]
+arma::vec gradCpp(arma::mat Kmat, arma::vec fhats, arma::vec Y, double rho, double lambda){
+  return Kmat*(exp((2-rho)*fhats)-Y%exp((1-rho)*fhats))+2*lambda*fhats;
 }
 
 // [[Rcpp::depends(RcppArmadillo)]]
 // [[Rcpp::export]]
 // matrix of second partial derivatives of the log-likelihood + penalty term
-arma::mat hessCpp(arma::mat Kmat, arma::vec alpha, arma::vec Y, double rho, double lambda){
-  arma::vec fhats = Kmat * alpha;
-  return Kmat * diagmat((2-rho)*exp((2-rho)*fhats)-(1-rho)*Y%exp((1-rho)*fhats))* Kmat + 2*lambda*Kmat;
+arma::mat hessCpp(arma::mat Kmat, arma::vec fhats, arma::vec Y, double rho, double lambda){
+  return Kmat * diagmat((2-rho)*exp((2-rho)*fhats)-(1-rho)*Y%exp((1-rho)*fhats))*Kmat + lambda*Kmat;
 }
 
 // [[Rcpp::depends(RcppArmadillo)]]
 // [[Rcpp::export]]
-arma::vec NRCpp(arma::mat Kmat, arma::vec alpha_start, arma::vec Y, double rho, double lambda){
-int n=Kmat.n_rows;
+arma::vec NRCpp(arma::mat Kmat, arma::vec alpha_start, arma::vec Y, double rho, double lambda, int n){
 arma::vec grad=arma::ones<arma::vec>(n);
 arma::vec update(n);
 arma::vec alpha=alpha_start;
+arma::vec fhats(n);
 double check = 1;
 int i=1;
 while(check > pow(10,-5)) {
-    grad = gradCpp(Kmat,alpha,Y,rho,lambda);
-    update = inv(hessCpp(Kmat,alpha,Y,rho,lambda))*grad;
+    fhats = Kmat*alpha;
+    grad = gradCpp(Kmat,fhats,Y,rho,lambda);
+    update = inv(hessCpp(Kmat,fhats,Y,rho,lambda))*grad;
     alpha -= update;
     check = accu(grad%grad);
     i+=1;
