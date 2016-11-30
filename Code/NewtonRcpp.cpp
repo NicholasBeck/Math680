bd91d3cf6e5abd661ad9cb1f87a0b880e9a9c92e
DOOM\Nicholas <nicholas.rudolph.beck@gmail.com>
2016-11-30 15:29:58 -0500

    dev

--- Code/NewtonRcpp.cpp
+++ Code/NewtonRcpp.cpp
@@ -17,8 +17,8 @@ arma::vec gradCpp(arma::mat Kmat, arma::vec fhats, arma::vec Y, double rho, doub
 // [[Rcpp::depends(RcppArmadillo)]]
 // [[Rcpp::export]]
 // matrix of second partial derivatives of the log-likelihood + penalty term
-arma::mat hessCpp(arma::mat Kmat, arma::vec fhats, arma::vec Y, double rho, double lambda){
-  return (Kmat * diagmat((2-rho)*exp((2-rho)*fhats)-(1-rho)*Y%exp((1-rho)*fhats)) + lambda)*Kmat;
+arma::mat hessCpp(arma::mat Kmat, arma::vec fhats, arma::vec Y, double rho, arma::mat dlam){
+  return (Kmat * diagmat((2-rho)*exp((2-rho)*fhats)-(1-rho)*Y%exp((1-rho)*fhats)) + dlam)*Kmat;
 }
 
 // [[Rcpp::depends(RcppArmadillo)]]
@@ -28,12 +28,13 @@ arma::vec grad=arma::ones<arma::vec>(n);
 arma::vec update(n);
 arma::vec alpha=alpha_start;
 arma::vec fhats(n);
+arma::mat dlam = lambda*eye(n,n);
 double check = 1;
 int i=1;
 while(check > pow(10,-5)) {
     fhats = Kmat*alpha;
     grad = gradCpp(Kmat,fhats,Y,rho,lambda);
-    update = inv(hessCpp(Kmat,fhats,Y,rho,lambda))*grad;
+    update = inv(hessCpp(Kmat,fhats,Y,rho,dlam))*grad;
     alpha -= update;
     check = accu(grad%grad);
     i+=1;
